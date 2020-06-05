1922704e902653c5fb380d458d770fe473e45fca
Anisa <anisahamohamed@gmail.com>
2020-06-05 11:07:53 -0400

    another simple node server

--- simpleHttpsServer.js
+++ simpleHttpsServer.js
@@ -1,15 +1,26 @@
-const http = require("http");
-const server = http.createServer(function (request, response) {
-  response.writeHead(
-    200,
+// const http = require("http");
+// //Create the server Object
+// const server = http
+//   .createServer((req, res) => {
+//     //Write Response
+//     res.write("I'm a node server");
+//     res.end();
+//   })
+//   .listen(5000, () => console.log("Node server running"));
 
-    {
-      "Content-Type": "text/plain",
-    }
-  );
-  response.end("New Webserver");
-});
-server.listen(3000);
+const http = require("http");
+const server = http
+  .createServer((request, response) => {
+    response.writeHead(
+      200,
+
+      {
+        "Content-Type": "text/plain",
+      }
+    );
+    response.end("New Webserver");
+  })
+  .listen(3000, () => console.log("Node server running"));
 const request = require("request");
 request("http://www.google.com", function (error, response, body) {
   console.log(body);
