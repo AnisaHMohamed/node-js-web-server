bca70e536de80d13e5fc57660c8f3217e79aee3a
Anisa <anisahamohamed@gmail.com>
2020-06-05 12:09:48 -0400

    about and home page

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 // const Book = require("./book");
 // const myBook = new Book("How to be great", "Anisa Mohamed");
 // myBook.info();
-const Logger = require("./logger");
-const logger = new Logger();
 
-logger.on("message", (data) => console.log("Called Listener:", data));
-logger.log("Hello World");
-logger.log("heyy girl");
-logger.log("haalloo");
+// const Logger = require("./logger");
+// const logger = new Logger();
+
+// logger.on("message", (data) => console.log("Called Listener:", data));
+// logger.log("Hello World");
+// logger.log("heyy girl");
+// logger.log("haalloo");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Home</h1>");
+  }
+});
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
