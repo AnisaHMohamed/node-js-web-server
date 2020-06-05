4b1b8e620e5d4f99bbce13f769735da56893070d
Anisa <anisahamohamed@gmail.com>
2020-06-05 10:52:35 -0400

    logger event example

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const Book = require("./book");
-const myBook = new Book("How to be great", "Anisa Mohamed");
-myBook.info();
+// const Book = require("./book");
+// const myBook = new Book("How to be great", "Anisa Mohamed");
+// myBook.info();
+const Logger = require("./logger");
+const logger = new Logger();
+
+logger.on("message", (data) => console.log("Called Listener:", data));
+logger.log("Hello World");
+logger.log("heyy girl");
+logger.log("haalloo");
