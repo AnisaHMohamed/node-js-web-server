bee43134887d733ef4120cb5c0cf5626002e348d
Anisa <anisahamohamed@gmail.com>
2020-06-05 12:34:02 -0400

    node routing to html pages

--- index.js
+++ index.js
@@ -15,8 +15,32 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>Home</h1>");
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+  if (req.url === "/about") {
+    fs.readFile(
+      path.join(__dirname, "public", "about.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+  if (req.url === "/api/users") {
+    const users = [
+      { name: "Anisa Mohamed", age: 29 },
+      { name: "Mohamed Anisa", age: 30 },
+    ];
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
   }
 });
 const PORT = process.env.PORT || 5000;
